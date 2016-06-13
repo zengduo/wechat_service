a1dfdf6d4d13ce5929f1d3362ea7af3e62d56e7f
zengduo <115622742@qq.com>
2016-06-13 14:28:38 +0800

    我的初次尝试

---
+++ application.py
@@ -0,0 +1,26 @@
+import hashlib
+
+from flask import Flask, request, make_response
+import hashlib
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+# def wechat_auth():
+#     if request.method == 'GET':
+#         token = ''
+#         query = request.args
+#         signature = query.get('signature', '')
+#         timestamp = query.get('timestamp', '')
+#         nonce = query.get('nonce', '')
+#         echostr = query.get('echostr', '')
+#         s = [timestamp. nonce, token]
+#         s.sort()
+#         s = ''.join(s)
+#         if signature == hashlib.sha1(s).hexdigest():
+#             return make_response(echostr)
+def hello():
+    return '<p>Hello World!</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
