b6ebe8ffbcf3dd2be9127247bdb3bcd0c0785bb1
zengduo <115622742@qq.com>
2016-06-13 18:27:57 +0800

    Update application.py

--- application.py
+++ application.py
@@ -6,21 +6,19 @@ app = Flask(__name__)
 DEBUG = True
 
 @app.route('/', methods=['GET', 'POST'])
-# def wechat_auth():
-#     if request.method == 'GET':
-#         token = ''
-#         query = request.args
-#         signature = query.get('signature', '')
-#         timestamp = query.get('timestamp', '')
-#         nonce = query.get('nonce', '')
-#         echostr = query.get('echostr', '')
-#         s = [timestamp. nonce, token]
-#         s.sort()
-#         s = ''.join(s)
-#         if signature == hashlib.sha1(s).hexdigest():
-#             return make_response(echostr)
-def hello():
-    return '<p>Hello World!</p>'
+def wechat_auth():
+    if request.method == 'GET':
+        token = 'vxoSJZUB7Qcj5lf6s4uLWIg'
+        query = request.args
+        signature = query.get('signature', '')
+        timestamp = query.get('timestamp', '')
+        nonce = query.get('nonce', '')
+        echostr = query.get('echostr', '')
+        s = [timestamp. nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if signature == hashlib.sha1(s).hexdigest():
+            return make_response(echostr)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
