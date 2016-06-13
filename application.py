b80bb340bd35d43aeb7ebc9485b6323f448e49ed
zengduo <115622742@qq.com>
2016-06-13 19:28:25 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,24 +1,19 @@
 from flask import Flask, request, make_response
-
+import hashlib
 
 app = Flask(__name__)
-
-DEBUG = True
 
 @app.route('/', methods=['GET', 'POST'])
 def wechat_auth():
     if request.method == 'GET':
-        token = 'vxoSJZUB7Qcj5lf6s4uLWIg'
+        token = 'your token'  
         query = request.args
         signature = query.get('signature', '')
         timestamp = query.get('timestamp', '')
         nonce = query.get('nonce', '')
         echostr = query.get('echostr', '')
-        s = [timestamp. nonce, token]
+        s = [timestamp, nonce, token]
         s.sort()
         s = ''.join(s)
-        if signature == hashlib.sha1(s).hexdigest():
+        if (hashlib.sha1(s).hexdigest() == signature):
             return make_response(echostr)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
