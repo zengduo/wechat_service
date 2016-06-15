0a4c8959b0d345c0661a761a54cd105b1eee26ec
zengduo <115622742@qq.com>
2016-06-15 23:42:37 +0800

    Update application.py

--- application.py
+++ application.py
@@ -18,5 +18,16 @@ def wechat_auth():
         if (hashlib.sha1(s).hexdigest() == signature):
             return make_response(echostr)
             
+    else:
+        xml_rec = ET.fromstring(request.data)
+        ToUserName = xml_rec.find("ToUserName").text
+        FromUserName = xml_rec.find("FromUserName").text
+        Content = xml_rec.find("Content").text
+        reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        re_msg = (reply % (FromUserName, ToUserName, str(int(time.time())), Content))
+        response = make_response(re_msg)
+        response.content_type = 'application/xml'
+        return response
+            
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
