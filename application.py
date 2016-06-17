cab2884ddcfd40a484819463a7f44b2958272ce8
zengduo <115622742@qq.com>
2016-06-17 12:28:22 +0800

    修改

--- application.py
+++ application.py
@@ -17,6 +17,7 @@ def index():
 
 @app.route('/weixin', methods=['GET', 'POST'])
 def wechat_auth():
+    print "wechat_auth"
     if request.method == 'GET':
         token = 'wodeweixintoken'
         query = request.args
@@ -31,7 +32,8 @@ def wechat_auth():
             # return render_template('first.html')
             return make_response(echostr)
 
-    else:
+    elif request.method == "POST":
+        print "get POST"
         xml_rec = ET.fromstring(request.data)
         tousername = xml_rec.find("ToUserName").text
         fromusername = xml_rec.find("FromUserName").text
@@ -39,28 +41,29 @@ def wechat_auth():
         msgTpye = xml_rec.find("MsgType").text
         reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
 
-        if content == u"天气":
-            url = "http://m.ip138.com/51/guangzhou/tianqi/"
-            headers = {
-                'Connection': 'Keep-Alive',
-                'Accept': 'text/html, application/xhtml+xml, */*',
-                'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'}
-            req = urllib2.Request(url, headers=headers)
-            opener = urllib2.urlopen(req)
-            html = opener.read().decode('utf-8')
-            rex = r'(?<=img src="/image/s[0-9].gif" alt=").{1,6}(?=" />)'
-            rexx = r'(?<=div class="temperature">).{5,15}(?=</div>)'
-            n = re.findall(rex, html)
-            m = re.findall(rexx, html)
-            str_wether = ""
-            for (i,j) in zip(m,n):
-                str_wether = str_wether + j + "  " + i + '\n'
-
-
-            newcontent = "最近五天天气：\n" + str_wether.encode('utf-8')
-
-            re_msg = (reply % (fromusername, tousername, str(int(time.time())), newcontent))
+        # if content == u"天气":
+        #     url = "http://m.ip138.com/51/guangzhou/tianqi/"
+        #     headers = {
+        #         'Connection': 'Keep-Alive',
+        #         'Accept': 'text/html, application/xhtml+xml, */*',
+        #         'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
+        #         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'}
+        #     req = urllib2.Request(url, headers=headers)
+        #     opener = urllib2.urlopen(req)
+        #     html = opener.read().decode('utf-8')
+        #     rex = r'(?<=img src="/image/s[0-9].gif" alt=").{1,6}(?=" />)'
+        #     rexx = r'(?<=div class="temperature">).{5,15}(?=</div>)'
+        #     n = re.findall(rex, html)
+        #     m = re.findall(rexx, html)
+        #     str_wether = ""
+        #     for (i,j) in zip(m,n):
+        #         str_wether = str_wether + j + "  " + i + '\n'
+        #
+        #
+        #     newcontent = "最近五天天气：\n" + str_wether.encode('utf-8')
+        #
+        #     re_msg = (reply % (fromusername, tousername, str(int(time.time())), newcontent))
+        re_msg = (reply % (fromusername, tousername, str(int(time.time())), content))
         return make_response(re_msg)
 
     print "Finished!"
